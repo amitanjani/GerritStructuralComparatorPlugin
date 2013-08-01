2a0e3a6c8f71d7389ab7ca2652ea2099ce856247
amitanjani <amit.anjani@imaginea.com>
2013-08-01 18:53:13 +0530

    deleted unnecessary file

--- StructuralComparator/src/main/java/com/imaginea/comparator/repo/Comparator.java
+++
@@ -1,7 +0,0 @@
-package com.imaginea.javaStructuralComparator.repo;
-
-import com.imaginea.javaStructuralComparator.domain.ComparisonResult;;
-
-public interface Comparator {
-	ComparisonResult compare(String actualFile, String expectedFile);
-}
